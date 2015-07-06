11f91bd715679b4ccf997265ffc927495d68ee82
benhysell <ben.hysell@gmail.com>
2015-07-05 22:15:21 -0400

    Windows Universal

---
+++ IHS.MvvmCross.Plugins.Keychain/IHS.MvvmCross.Plugins.Keychain.Universal/Plugin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cirrious.CrossCore;
+using Cirrious.CrossCore.Plugins;
+
+namespace IHS.MvvmCross.Plugins.Keychain.Universal
+{
+    public class Plugin : IMvxPlugin
+    {
+        public void Load()
+        {
+            Mvx.RegisterType<IKeychain, WindowsUniversalKeychain>();
+        }
+    }
+}
---
+++ IHS.MvvmCross.Plugins.Keychain/IHS.MvvmCross.Plugins.Keychain.Universal/WindowsUniversalKeychain.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Windows.Security.Credentials;
+
+namespace IHS.MvvmCross.Plugins.Keychain.Universal
+{
+    public class WindowsUniversalKeychain : IKeychain
+    {
+        public bool SetPassword(string password, string serviceName, string account)
+        {
+            var returnValue = false;
+            try
+            {
+                // 
+                //Add a credential to PasswordVault by supplying resource, username, and password 
+                // 
+                var vault = new PasswordVault();
+                var credential = new PasswordCredential(serviceName, account, password);
+                vault.Add(credential);
+                returnValue = true;
+            }
+            catch (Exception)
+            {
+                returnValue = false;
+            }                      
+            return returnValue;
+        }
+
+        public string GetPassword(string serviceName, string account)
+        {
+            var returnValue = string.Empty;
+            try
+            {
+                // 
+                //Read a credential from PasswordVault by supplying resource or username 
+                // 
+                var vault = new PasswordVault();
+                PasswordCredential credential = null;
+
+                //Read by explicit resource and username name, result will be a single credential if it exists. Password will be returned. 
+                // 
+                credential = vault.Retrieve(serviceName, account);
+                returnValue = credential.Password;
+            }
+            catch (Exception)
+            {
+ 
+            }
+            return returnValue;
+        }
+
+        public bool DeletePassword(string serviceName, string account)
+        {
+            var returnValue = false;
+            try
+            {
+                // 
+                //Add a credential to PasswordVault by supplying resource, username, and password 
+                // 
+                var vault = new PasswordVault();
+                var credential = new PasswordCredential(serviceName, account, string.Empty);
+                vault.Add(credential);
+                returnValue = true;
+            }
+            catch (Exception)
+            {
+                returnValue = false;
+            }
+            return returnValue;
+        }
+
+        public LoginDetails GetLoginDetails(string serviceName)
+        {
+            var returnValue = new LoginDetails();
+
+            try
+            {                
+                var vault = new PasswordVault();
+                IReadOnlyList<PasswordCredential> credentials = null;              
+                credentials = vault.FindAllByResource(serviceName);
+                                               
+                if (null != credentials)
+                {                    
+                    foreach (var credential in credentials)
+                    {                        
+                        try
+                        {
+                            var credentialWithPassword = vault.Retrieve(credential.Resource, credential.UserName);
+                            returnValue.Password = credentialWithPassword.Password;
+                            returnValue.Username = credentialWithPassword.UserName;
+                        }
+                        catch (Exception)
+                        {                           
+                        }
+                    }
+                }
+            }
+            catch (Exception) // No stored credentials, so none to delete 
+            {
+                
+            }
+            return returnValue;
+        }
+
+        public bool DeleteAccount(string serviceName, string account)
+        {
+            var returnValue = false;
+            var vault = new PasswordVault();
+            var credential = vault.Retrieve(serviceName, account);
+            try
+            {
+                vault.Remove(credential);
+                returnValue = true;
+            }
+            catch (Exception)
+            {
+
+            }
+            return returnValue;
+        }
+    }
+}
